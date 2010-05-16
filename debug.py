6df91a38e6bbfa1c2b1d6b3eb18291c565f67587
Lakshman Prasad <lakshman@uswaretech.com>
2010-05-16 16:16:08 +0530

    A better debug

--- debug.py
+++ debug.py
@@ -1,5 +1,14 @@
-#from IPython.Shell import IPShellEmbed
-#ipython = IPShellEmbed()
-import ipdb
-idebug = ipdb.set_trace
-    
+def nothing():
+    pass
+
+try:
+    from IPython.Shell import IPShellEmbed
+    ipython = IPShellEmbed()
+except:
+    ipython = nothing
+
+try:
+    import ipdb
+    idebug = ipdb.set_trace
+except:
+    idebug = nothing
